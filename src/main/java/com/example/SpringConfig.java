2170787d5c25dcf6a0fd6d90183a98bed2e09dcb
bugpigg <bugpigg@gmail.com>
2022-03-08 22:38:24 +0900

    refact : 순수 JDBC 를 사용하여 H2 DB 구축 및 연결
    - build.gradle H2 DB 관련 의존성 추가 및 SpringConfig 설정 추가
    - JdbcMemberRepository 클래스 추가
    - application.properties 추가

--- src/main/java/com/example/SpringConfig.java
+++ src/main/java/com/example/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import com.example.repository.JdbcMemberRepository;
 import com.example.repository.MemberRepository;
 import com.example.repository.MemoryMemberRepository;
 import com.example.service.MemberService;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired // spring 이 application.properties 보고 미리 datasource 를 빈으로 만들어 둔다. 그래서 이렇게 주입 가능하다.
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
