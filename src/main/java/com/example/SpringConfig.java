bd4933c1c6c163ba15aa438bbefa41d2384e36a8
bugpigg <bugpigg@gmail.com>
2022-03-09 23:58:16 +0900

    feat : 스프링 JDBC 템플릿으로 변환
    - SpringConfig 수정
    - JdbcTemplateMemberRepository 클래스 생성

--- src/main/java/com/example/SpringConfig.java
+++ src/main/java/com/example/SpringConfig.java
@@ -1,9 +1,12 @@
 package com.example;
 
 import com.example.repository.JdbcMemberRepository;
+import com.example.repository.JdbcTemplateMemberRepository;
+import com.example.repository.JpaMemberRepository;
 import com.example.repository.MemberRepository;
 import com.example.repository.MemoryMemberRepository;
 import com.example.service.MemberService;
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +15,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//
+//    @Autowired // spring 이 application.properties 보고 미리 datasource 를 빈으로 만들어 둔다. 그래서 이렇게 주입 가능하다.
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    @Autowired // spring 이 application.properties 보고 미리 datasource 를 빈으로 만들어 둔다. 그래서 이렇게 주입 가능하다.
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -26,6 +36,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
